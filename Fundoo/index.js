280f03816199ce904b1229f08d7ff123fd31b039
akshay <akshay.yamgar29@gmail.com>
2022-01-09 17:58:33 +0530

    [akshay]: Added Swagger file

--- Fundoo/index.js
+++ Fundoo/index.js
@@ -7,9 +7,16 @@ const database = require("./config/database");
 global.TextEncoder = require("util").TextEncoder;
 const router = require("./router/router");
 
+const swaggerJSdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = require("../swagger.json");
+
 // start your app
 const app = express();
 // var expressValidator = require('express-validator');
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 // app.use(expressValidator());
